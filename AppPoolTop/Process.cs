52c75d467d34b56cedd9ebe3b775ceaa6607caf8
Kent Gustavsson <kent@minoris.se>
2013-05-14 19:11:27 +0200

    Initital commit

---
+++ AppPoolTop/Process.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace ConsoleApplication5
+{
+    class Process
+    {
+        string _instanceName;
+        string _appPoolName;
+        PerformanceCounter _cpuCounter;
+        int _pid;
+        double _nextValue = 0;
+
+        public Process(string instance, int pid)
+        {
+            _instanceName = instance;
+            _pid = pid;
+            _cpuCounter = new PerformanceCounter("Process", "% Processor Time", _instanceName, true);
+        }
+
+        public int pid
+        {
+            get
+            {
+                return _pid;
+            }
+        }
+
+        public string instanceName 
+        {
+            get
+            {
+                return _instanceName;
+            }
+        }
+
+        public PerformanceCounter cpuCounter
+        {
+            get
+            {
+                return _cpuCounter;
+            }
+        }
+
+        public string appPoolName
+        {
+            get
+            {
+                return _appPoolName;
+            }
+            set
+            {
+                _appPoolName = value;
+            }
+        }
+
+        public void step()
+        {
+            _nextValue = cpuCounter.NextValue();
+        }
+
+        public double nextValue
+        {
+            get
+            {
+                return _nextValue;
+            }
+        }
+    }
+}
---
+++ AppPoolTop/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static List<Process> counters = new List<Process>();
+        static void Main(string[] args)
+        {
+
+
+
+            PerformanceCounterCategory cat = new PerformanceCounterCategory("Process");
+            string[] instances = cat.GetInstanceNames();
+            foreach (string instance in instances)
+            {
+
+                using (PerformanceCounter cnt = new PerformanceCounter("Process",
+                     "ID Process", instance, true))
+                {
+                    if (instance.StartsWith("w3wp"))
+                    {
+                        Process process = new Process(instance, (int)cnt.RawValue);
+                        counters.Add(process);
+
+                        Console.WriteLine(instance + ": " + ((int)cnt.RawValue));
+                        int val = (int)cnt.RawValue;
+
+                        WqlObjectQuery wqlQuery = new WqlObjectQuery("SELECT * from Win32_Process where ProcessId=" + val);
+                        ManagementObjectSearcher searcher = new ManagementObjectSearcher(wqlQuery);
+
+                        foreach (ManagementObject disk in searcher.Get())
+                        {
+                            Regex re = new Regex("-ap \"(.+)\"");
+                            process.appPoolName = re.Match(disk.GetPropertyValue("CommandLine").ToString()).Value.Substring(5);
+                            process.appPoolName = process.appPoolName.Substring(0, process.appPoolName.Length - 1);
+                            Console.WriteLine(process.appPoolName);
+                        }
+                    }
+                }
+            }
+
+            int maxNameLen = 0;
+            foreach (Process process in counters)
+            {
+                if (process.appPoolName.Length > maxNameLen)
+                {
+                    maxNameLen = process.appPoolName.Length;
+                }
+            }
+
+
+            while (!Console.KeyAvailable)
+            {
+                foreach (Process myAppCpu in counters)
+                {
+                    myAppCpu.step();
+                }
+
+                Console.Clear();
+
+                foreach (Process myAppCpu in counters.OrderByDescending(x => x.nextValue))
+                {
+                    Console.Write(myAppCpu.appPoolName);
+                    for (int i = 0; i < maxNameLen - myAppCpu.appPoolName.Length ; i++)
+			        {
+			            Console.Write(" ");
+			        }
+                    Console.WriteLine(" CPU % = " + myAppCpu.nextValue);
+                }
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
